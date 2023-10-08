dc6b117471c068afb4de42547abd868b76ccae81
Mark Mazur <mark.mazur007@gmail.com>
2023-10-08 02:43:04 -0400

    create and add logic for vars

---
+++ vars.py
@@ -0,0 +1,7 @@
+class Const:
+    WIDTH = 10 # wigth of the world
+    HEIGHT = 10 # height of the world
+    CLEAR_SCREEN_VALUE = 10
+
+class Char:
+    EMPTY = '.' # symbol that will fill the whole world
