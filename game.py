72ae8dd8e592c61b058f4b7551285c9a626e44e7
Mark Mazur <mark.mazur007@gmail.com>
2023-10-08 03:00:12 -0400

    add 2 methods

--- game.py
+++ game.py
@@ -3,10 +3,15 @@ from vars import *
 
 class Game:
     world = [[Char.EMPTY for i in range(Const.WIDTH)] for i in range(Const.HEIGHT)] # 2d world
-    info = ''
+    info = '' # this var will be under print world. Info for set some text (informations)
 
     @staticmethod
-    def print_world():
+    def print_world(): # print world. Wigth by height
+        Game.__clear_screen()
         for i in reversed(Game.world):
             print(*i)
         print(Game.info)
+    
+    @staticmethod
+    def __clear_screen():
+        print('\n' * Const.CLEAR_SCREEN_VALUE)
