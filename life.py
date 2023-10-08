8f193748cb24aa86fc9dbef0941b0d8936eb57f8
Mark Mazur <mark.mazur007@gmail.com>
2023-10-08 03:43:11 -0400

    add loop, check, brain and die methods

--- life.py
+++ life.py
@@ -8,6 +8,7 @@ class Life:
         self.name = name
         self.symbol = symbol
         self.x, self.y = x, y
+        self.satiety = 15
         self.alive = alive
         self.random_start_pos = random_start_pos
         
@@ -15,7 +16,17 @@ class Life:
     
     def loop(self):
         if self.alive:
-            pass
+            self.brain()
+            self.check()
+    
+    def check(self):
+        if self.satiety <= 0:
+            self.die()
+        else:
+            self.satiety -= 1
+
+    def brain(self):
+        pass
         
     def set_on_map(self, new_x: int, new_y: int, anyway: bool = False):
         Game.set_on_map(new_x, new_y, self.symbol, anyway)
@@ -23,3 +34,7 @@ class Life:
     def random_start_pos(self):
         new_x, new_y = randint(0, Const.WIDTH-1), randint(0, Const.HEIGHT-1)
         self.set_on_map(new_x, new_y, anyway=True)
+    
+    def die(self):
+        self.alive = False
+        del self
