e05d2c3266fe1f8532e4bced6d31c64cb93a862a
Mark Mazur <mark.mazur007@gmail.com>
2023-10-08 03:12:29 -0400

    add main methods to life

--- life.py
+++ life.py
@@ -1,4 +1,5 @@
 from vars import *
+from random import randint
 
 
 class Life:
@@ -8,3 +9,16 @@ class Life:
         self.x, self.y = x, y
         self.alive = alive
         self.random_start_pos = random_start_pos
+        
+        if self.random_start_pos: self.random_start_pos()
+    
+    def loop(self):
+        if self.alive:
+            pass
+        
+    def set_on_map(self, new_x: int, new_y: int, anyway: bool = False):
+        pass
+    
+    def random_start_pos(self):
+        new_x, new_y = randint(0, Const.WIDTH-1), randint(0, Const.HEIGHT-1)
+        self.set_on_map(new_x, new_y, anyway=True)
