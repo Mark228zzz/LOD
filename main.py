0310d0da5e97c7ed3d0fbabb0b7fb203351a0707
Mark Mazur <mark.mazur007@gmail.com>
2023-10-15 02:52:11 -0400

    add color to obstacle

--- main.py
+++ main.py
@@ -50,9 +50,10 @@ class Game:
 
 
 class Obstacle:
-    def __init__(self, x: int, y: int, width: int = 1, height: int = 1):
+    def __init__(self, x: int, y: int, width: int = 1, height: int = 1, color: tuple = (150, 75, 0)):
         self.x, self.y = x, y
         self.width, self.height = width, height
+        self.color = color
         
         Game.list_of_obstacles.append(self)
 
