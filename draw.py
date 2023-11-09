ef16873f429b5224068ebf1f9b588b45fb58b4bb
Mark Mazur <mark.mazur007@gmail.com>
2023-11-09 11:42:52 -0500

    edit window

--- draw.py
+++ draw.py
@@ -6,8 +6,6 @@ from vars import *
 class Draw:
     @staticmethod
     def all():
-        Draw.debug_window()
-
         for animal in List.animals: # draw all animals that are alive
             pygame.draw.rect(Game.window, animal.color, (animal.x, animal.y, animal.width, animal.height))
         for food in List.foods: # draw all foods that wasn`t eatten
@@ -15,12 +13,15 @@ class Draw:
         for predator in List.predators: # draw all predators that are alive
             pygame.draw.rect(Game.window, predator.color, (predator.x, predator.y, predator.width, predator.height))
 
+        Draw.debug_window()
+
     @staticmethod
     def debug_window():
-        font = pygame.font.SysFont('arial', 10)
-        info_animal = font.render(f'Animals: {len(List.animals)}', True, (0, 0, 0), (200, 200, 200))
-        info_food = font.render(f'Foods: {len(List.foods)}', True, (0, 0, 0), (200, 200, 200))
+        y_pos = 5
+        font = pygame.font.SysFont('arial', 14)
+        info_animal   = font.render(f'Animals  : {len(List.animals)}', True, (0, 0, 0), (200, 200, 200))
+        info_food     = font.render(f'Foods    : {len(List.foods)}', True, (0, 0, 0), (200, 200, 200))
         info_predator = font.render(f'Predators: {len(List.predators)}', True, (0, 0, 0), (200, 200, 200))
-        Game.window.blit(info_animal, (5, 5))
-        Game.window.blit(info_food, (5, 15))
-        Game.window.blit(info_predator, (5, 25))
+        Game.window.blit(info_animal, (5, y_pos+14))
+        Game.window.blit(info_food, (5, y_pos+28))
+        Game.window.blit(info_predator, (5, y_pos+42))
