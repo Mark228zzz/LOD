e638e365df8bd64c735f1d6f988fde6de806d225
Mark Mazur <mark.mazur007@gmail.com>
2023-11-12 04:37:47 -0500

    fix debaug window text

--- windows/debug_window.py
+++ windows/debug_window.py
@@ -19,7 +19,7 @@ class DebugWindow(Window):
         if not self.visible: return # checking if the game can draw this window
         font = pygame.font.SysFont('arial', 14)
         info_animal   = font.render(f'Animals  : {len(List.animals)}', True, (0, 0, 0), (200, 200, 200))
-        info_food     = font.render(f'Foods    : {len(List.foods)}', True, (0, 0, 0), (200, 200, 200))
+        info_food     = font.render(f'Food     : {len(List.foods)}', True, (0, 0, 0), (200, 200, 200))
         info_predator = font.render(f'Predators: {len(List.predators)}', True, (0, 0, 0), (200, 200, 200))
         self.game_window.blit(info_animal, (5, 14))
         self.game_window.blit(info_food, (5, 28))
